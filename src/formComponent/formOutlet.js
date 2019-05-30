a14124860eaa7b24e415ab540f27531cfd28cfd5
Srikanth ek06 <srikanthek06@hotmail.com>
2019-05-29 20:00:24 -0700

    Initial commit

--- src/formComponent/formOutlet.js
+++ src/formComponent/formOutlet.js
@@ -4,24 +4,54 @@ export default class FormOutlet extends React.Component {
   constructor() {
     super();
     this.state = {
-      firstname: ""
+      firstname: "",
+      lastname: ""
     };
     this.onChange = this.onChange.bind(this);
   }
   onChange(event) {
     const { name, value, type, check } = event.target;
-    console.log(type);
+    this.setState({ [name]: value });
+  }
+  componentDidMount() {
+    fetch("https://randomuser.me/api/")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.results[0]);
+        let user = data.results[0];
+        this.setState({
+          firstname: user.name.first,
+          lastname: user.name.last
+        });
+      });
   }
   render() {
     return (
       <form>
-        <input
-          type="text"
-          name="firstname"
-          value={this.state.firstname}
-          placeholder="First Name"
-          onChange={this.onChange}
-        />
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstname"
+            value={this.state.firstname}
+            placeholder="First Name"
+            onChange={this.onChange}
+          />
+        </label>
+        <br />
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastname"
+            value={this.state.lastname}
+            placeholder="Last Name"
+            onChange={this.onChange}
+          />
+        </label>
+        <br />
+        Your name: {this.state.firstname} {this.state.lastname}
       </form>
     );
   }
